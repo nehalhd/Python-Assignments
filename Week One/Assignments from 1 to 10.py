d08cb7ce2e63aa26b526b31d64cc00ac940fbaf8
nehalhd <nehalhdev@gmail.com>
2022-04-12 06:05:24 +0200

    first commit

---
+++ Week One/Assignments from 1 to 10.py
@@ -0,0 +1,18 @@
+# # 2
+# name = 'nehal'
+# age = "26"
+# country = "Egypt"
+
+
+# # 3 
+# print("name :" + name)
+# print("age :" + age)
+# print("Country :" + country)
+
+# #4
+# print("Hello, My Name Is " + name + " And Iam " + age + "  Years Old and I Live in " + country )
+
+# #5
+# print(type(name))
+# print(type(age))
+# print(type(country))
---
+++ Week Two/assignments from 10 to 18.py
@@ -0,0 +1,91 @@
+
+# #1
+# name = 'nehal'
+# age = 26
+# country = "Egypt"
+
+# print(f"\"Hello \' {name}\' , How You Doing \\ \"\"\" Your Age Is \"{age}\" + And Your Country Is: {country}")
+
+# #2
+# print(f"\"Hello \' {name}\' , How You Doing \\ \n \"\"\" Your Age Is \"{age}\" + \n And Your Country Is: {country}")
+
+# #3
+# name = 'Elzero'
+
+# print(name[1])
+# print(name[2])
+# print(name[-1])
+
+# print(name[1:2])
+# print(name[2:3])
+# print(name[-1::1])
+
+# #4
+# print(name[1:4])
+# print(name[0::2])
+# print(name[-2::-2])
+
+# #5
+# name = "#@#@Elzero#@#@"
+
+# print(name.strip("#@"))
+
+
+# #6
+# num = "9"
+# print(num.zfill(4))
+# num = "15"
+# print(num.zfill(4))
+# num = "130"
+# print(num.zfill(4))
+# num = "950"
+# print(num.zfill(4))
+# num = "1500"
+# print(num.zfill(4))
+
+
+
+# #7
+# name_one = "Osama"
+# name_two = "Osama_Elzero"
+# addchar = name_one.center(30, "@")
+# print(addchar.rstrip("@"))
+# addcharr = name_two.center(22, "@")
+# print(addcharr.rstrip("@"))
+# #or
+# print(name_one.rjust(20, "@"))
+# print(name_two.rjust(20, "@"))
+
+# #8
+# name_one = "OSamA"
+# name_two = "osaMA"
+
+# print(name_one.swapcase())
+# print(name_two.swapcase())
+
+# #9 
+# msg = "I Love Python And Although Love Elzero Web School"
+
+# print(msg.count("Love"))
+
+
+# #10
+# name = "Elzero"
+# print(name.index("z"))
+
+
+# #11
+# msg = "I <3 Python And Although <3 Elzero Web School"
+
+# print(msg.replace("<3", "love" , 1))
+
+#12
+# #11
+# msg = "I <3 Python And Although <3 Elzero Web School"
+# print(msg.replace("<3", "love"))
+
+# #13
+# name = "nehal"
+# age = 26
+# country = "Egypt"
+# print(f" My Name Is {name}, And My Age Is {age}, And My Country Is {country}")
